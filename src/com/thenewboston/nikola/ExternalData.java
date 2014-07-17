4a0645e6c792410fee561e4745a79a57dcf3da78
Nikola Curilovic <ncurilovic@gmail.com>
2014-07-17 17:33:37 +0200

    110 tutorial - Media Scanner Connection

--- src/com/thenewboston/nikola/ExternalData.java
+++ src/com/thenewboston/nikola/ExternalData.java
@@ -1,8 +1,15 @@
 package com.thenewboston.nikola;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.app.Activity;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -14,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExternalData extends Activity implements OnItemSelectedListener, OnClickListener  {
 
@@ -23,6 +31,7 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 	Spinner spinner;
 	String[] paths = {"Music", "Picture", "Download"};
 	File path = null;
+	File file = null;
 	EditText saveFile;
 	Button confrim, save;
 	
@@ -36,11 +45,23 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 		canRead = (TextView) findViewById(R.id.tvCanRead);
 		confrim = (Button) findViewById(R.id.bConfrimSaveAs);
 		save = (Button) findViewById(R.id.bSaveFile);
+		saveFile = (EditText) findViewById(R.id.etSaveAs);
 		
 		confrim.setOnClickListener(this);
 		save.setOnClickListener(this);
 		
+		checkState();
 		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
+		
+		
+	}
+
+	private void checkState() {
+		// TODO Auto-generated method stub
 		state = Environment.getExternalStorageState();
 		if (state.equals(Environment.MEDIA_MOUNTED)) {
 			// read/write
@@ -59,13 +80,6 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 			canRead.setText("false");
 			canW = canR = false;
 		}
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths);
-		spinner = (Spinner) findViewById(R.id.spinner1);
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(this);
-		
-		
 	}
 
 	@Override
@@ -97,6 +111,44 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 		// TODO Auto-generated method stub
 		switch (arg0.getId()) {
 		case R.id.bSaveFile:
+			String f = saveFile.getText().toString();
+			file = new File(path, f + ".png");
+			
+			checkState();
+			if(canW == canR == true){
+				
+				path.mkdirs();
+				
+				try {
+					InputStream is = getResources().openRawResource(R.drawable.greenball);
+					OutputStream os = new FileOutputStream(file);
+					byte[] data = new byte[is.available()];
+					is.read(data);
+					os.write(data);
+					is.close();
+					os.close();
+					
+					Toast t = Toast.makeText(ExternalData.this, "File has been Saved", Toast.LENGTH_LONG);
+					t.show();
+					// Update files for the user to use
+					MediaScannerConnection.scanFile(ExternalData.this, new String[] {file.toString()}, null, new MediaScannerConnection.OnScanCompletedListener() {
+						
+						@Override
+						public void onScanCompleted(String path, Uri uri) {
+							// TODO Auto-generated method stub
+							Toast t = Toast.makeText(ExternalData.this, "scan complete", Toast.LENGTH_SHORT);
+							t.show();
+						}
+					});
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			
 			break;
 		case R.id.bConfrimSaveAs:
@@ -106,3 +158,12 @@ public class ExternalData extends Activity implements OnItemSelectedListener, On
 	}
 
 }
+
+
+
+
+
+
+
+
+
